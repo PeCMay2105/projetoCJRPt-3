542be66ee4f4baf78d7fb6e17db55869ed7cc7a2
jonjonvps <joaovictorps28@gmail.com>
2023-11-17 20:05:01 -0300

    Problem em conectar com o prisma

--- backEnd/model/publicacoes.js
+++ backEnd/model/publicacoes.js
@@ -1,22 +1,12 @@
-import { Post } from "./Post.js"
-import { Usuario } from "./Usuario.js"
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
- export class publicacoes{
+const prisma = new PrismaClient();
+
+export class publicacoes{
     usuarios = []
     posts = []
-    novoUsuario(Nome,email,senha,foto,bio){
-
-        const criaUsuario = ()=>{
-            if(this.usuarios[email]){
-                throw new Error("Já existe um usuario cadastrado para esse email")
-            }
-            else{
-                NovoUsuario = new Usuario(Nome,email,senha,foto,bio)
-                this.usuarios[email] = NovoUsuario
-                return novoUsuario
-            }
-        }
-    }
+    
 
     novoPost(usuario,texto,temImg,imagem){
         const criaPost = ()=>{
@@ -25,9 +15,7 @@ import { Usuario } from "./Usuario.js"
 
     }
 
-    listaUsuarios(){
-        return usuarios
-    }
+    
     listaPosts(){
         return posts
     }
