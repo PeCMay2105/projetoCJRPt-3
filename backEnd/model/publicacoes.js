6d1063d8bee097dbbe41c582e2ef541cb66853a1
jonjonvps <joaovictorps28@gmail.com>
2023-11-17 20:11:54 -0300

    Problem em conectar com o prisma

--- backEnd/model/publicacoes.js
+++ backEnd/model/publicacoes.js
@@ -1,7 +1,6 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-
+import { PrismaClient }  from '@prisma/client';
 const prisma = new PrismaClient();
+
 
 export class publicacoes{
     usuarios = []
