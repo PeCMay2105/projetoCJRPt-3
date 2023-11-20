d491de368da4c7f009ed5b65c3fc7600c1c989f6
PeCMay2105 <pedrocmaia2105@gmail.com>
2023-11-20 13:10:07 -0300

    backEnd

--- backEnd/model/Usuario.js
+++ backEnd/model/Usuario.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 
 export default class Usuario{
@@ -9,62 +9,5 @@ export default class Usuario{
         this.foto = foto
         this.bio = bio
         this.adm = adm
-=======
-import { PrismaClient }  from '@prisma/client';
-const prisma = new PrismaClient();
->>>>>>> 31559985aac5e596cfc4b8b05cd16bb8af82345f
-
-export class Usuario {
-    async novoUsuario(nome, email, senha, foto, bio) {
-        return await prisma.Usuario.create({
-            data: {
-                nome,
-                email,
-                senha,
-                foto,
-                bio
-            },
-        }).catch(e => {
-            if (e.code === 'P2002')
-                throw new Error("Já existe um usuario cadastrado para esse email");
-
-            throw e;
-        });
     }
-<<<<<<< HEAD
-   
-
-
 }
-=======
-
-    async listaUsuarios() {
-        return await prisma.Usuario.findMany();
-    }
-
-    async removerUsuario(id) {
-        return await prisma.Usuario.delete({
-            where: {
-                id
-            }
-        }).catch(e => {
-            if (e.code === 'P2025')
-                throw new Error('Produto não encontrado')
-            throw e;
-        })
-    }
-
-    /*
-        const criaUsuario = ()=>{
-            if(this.usuarios[email]){
-                throw new Error("Já existe um usuario cadastrado para esse email")
-            }
-            else{
-                NovoUsuario = new Usuario(Nome,email,senha,foto,bio)
-                this.usuarios[email] = NovoUsuario
-                return novoUsuario
-            }
-        }
-    */
-}
->>>>>>> 31559985aac5e596cfc4b8b05cd16bb8af82345f
